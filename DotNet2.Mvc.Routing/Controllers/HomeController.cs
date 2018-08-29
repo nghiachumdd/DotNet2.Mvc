cee5fc51d16fb556a6e8d325fce12b4aea4f7ef1
Nghia Lai <laitrongnghia0606@gmail.com>
2018-08-29 18:25:59 +0700

    Using route basis in mvc

---
+++ DotNet2.Mvc.Routing/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet2.Mvc.Routing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("This is index page");
+        }
+
+        public IActionResult PageOne()
+        {
+            return Content("This is page one");
+        }
+
+        //[HttpGet]
+        public IActionResult PageTwo()
+        {
+            return Content("This is page two");
+        }
+
+        [HttpPost]
+        public IActionResult PageTwo(int id)
+        {
+            return Content($"(Post) This is page two: {id}");
+        }
+    }
+}
